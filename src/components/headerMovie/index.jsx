2a14b96d9746780483e968ffa5a79d30bb9510b5
Isadora Fitzgerald <fitzdora@gmail.com>
2023-06-17 15:47:42 +0100

    Trying to get favourite icon change on header page

--- src/components/headerMovie/index.jsx
+++ src/components/headerMovie/index.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Avatar from "@mui/material/Avatar";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+
 
 const styles = {
     root: {  
@@ -14,30 +17,73 @@ const styles = {
     flexWrap: "wrap",
     padding: 1.5,
   },
+  avatar: {
+    backgroundColor: "rgb(255, 0, 0)",
+  }
 };
 
 const MovieHeader = (props) => {
   const movie = props.movie;
+  const [favourites, setFavourites] = useState([]);
+  
+  // retrieve movies from locals storage
+  useEffect(() => {
+    const storedFavourites = localStorage.getItem("updatedMovies");
+    if (storedFavourites) {
+      setFavourites(JSON.parse(storedFavourites));
+    }
+  }, []);
 
-  return (
+  // set error handling
+  const handleAddToFavourite = (e) => {
+    e.preventDefault();
+
+
+  // Check if the current movie is a favorite
+  const isFavorite = favourites.includes(movie.id);
+
+
+    // Update the list of favorite movies
+    let updatedFavourites;
+    if (isFavorite) {
+      updatedFavourites = favourites.filter((id) => id !== movie.id);
+    } else {
+      updatedFavourites = [...favourites, movie.id];
+    }
+    
+    setFavourites(updatedFavourites);
+
+    // save the update favourites to local storage
+    localStorage.setItem("updatedMovies", JSON.stringify(updatedFavourites));
+  
+  };
+  const isFavorite = favourites.includes(movie.id);
+   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
+          <Avatar sx={styles.avatar}>
+          {isFavorite ? (
+          <FavoriteIcon color="disabled" />
+        ) : (
+          <FavoriteIcon />
+        )}
+          </Avatar>
+
       <Typography variant="h4" component="h3">
         {movie.title}{"   "}
         <a href={movie.homepage}>
-          <HomeIcon color="primary"  fontSize="='large"/>
+          <HomeIcon color="primary"  fontSize="large"/>
         </a>
         <br />
         <span>{`${movie.tagline}`} </span>
-      </Typography>
+      </Typography> 
       <IconButton aria-label="go forward">
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
   );
 };
-
 export default MovieHeader;
