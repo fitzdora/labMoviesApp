dec6768d3fac3ef9533837c2acf0f2b9fd19d262
Isadora Fitzgerald <fitzdora@gmail.com>
2023-06-18 11:27:22 +0100

    still trying excercise 3 to work

--- src/components/headerMovie/index.jsx
+++ src/components/headerMovie/index.jsx
@@ -8,85 +8,74 @@ import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
-
 const styles = {
-    root: {  
+  root: {
     display: "flex",
     justifyContent: "space-around",
     alignItems: "center",
     flexWrap: "wrap",
     padding: 1.5,
   },
-  /* avatar: {
+  avatar: {
+    color: "disabled",
+  },
+  avatarFavorite: {
     color: "rgb(255, 0, 0)",
-  } */
+  },
 };
 
 const MovieHeader = (props) => {
   const movie = props.movie;
-  // const [favourites, setFavourites] = useState([]);
-  
-  // retrieve movies from locals storage
+  const [favourites, setFavourites] = useState([]);
 
-  const favourites = JSON.parse(localStorage.getItem('favourites') || []);
-  /* useEffect(() => {
-    const storedFavourites = localStorage.getItem("updatedMovies");
-    if (storedFavourites) {
-      setFavourites(JSON.parse(storedFavourites));
-    }
-  }, []); */
+  useEffect(() => {
+    const storedFavourites = JSON.parse(localStorage.getItem("favourites")) || [];
+    setFavourites(storedFavourites);
+  }, []);
 
-  // set error handling
-  const handleAddToFavourite = (e) => {
-    e.preventDefault();
-
-
-  // Check if the current movie is a favorite
-  const isFavorite = favourites.includes(movie.id);
-
-
-    // Update the list of favorite movies
-    /* let updatedFavourites;
+  const handleAddToFavourite = () => {
+    const isFavorite = favourites.includes(movie.id);
+    let updatedFavourites;
     if (isFavorite) {
       updatedFavourites = favourites.filter((id) => id !== movie.id);
     } else {
       updatedFavourites = [...favourites, movie.id];
     }
-    
-    setFavourites(updatedFavourites); */
 
-    // save the update favourites to local storage
-    //localStorage.setItem("updatedMovies", JSON.stringify(updatedFavourites));
-  
+    setFavourites(updatedFavourites);
+    localStorage.setItem("favourites", JSON.stringify(updatedFavourites));
   };
-  const isFavorite = favourites.includes(movie.id);
 
-   return (
+  const isFavorite = favourites.includes(movie.id);
+  const avatarStyle = isFavorite ? styles.avatarFavorite : styles.avatar;
+
+  return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
-          <Avatar sx={styles.avatar}>
-          {isFavorite ? (
-          <FavoriteIcon color="disabled" />
+      <Avatar sx={avatarStyle}>
+        {isFavorite ? (
+          <FavoriteIcon color="inherit" />
         ) : (
           <FavoriteIcon />
         )}
-          </Avatar>
+      </Avatar>
 
       <Typography variant="h4" component="h3">
         {movie.title}{"   "}
         <a href={movie.homepage}>
-          <HomeIcon color="primary"  fontSize="large"/>
+          <HomeIcon color="primary" fontSize="large" />
         </a>
         <br />
         <span>{`${movie.tagline}`} </span>
-      </Typography> 
+      </Typography>
       <IconButton aria-label="go forward">
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
   );
 };
+
 export default MovieHeader;
